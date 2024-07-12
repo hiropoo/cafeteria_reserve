7241238138ea7ba0314acd982f7a6c4ab3bb5ed4
hiropoo <125180473+hiropoo@users.noreply.github.com>
2024-07-12 17:06:14 +0900

    ✨ ログインページにて、ログイン処理とそのエラー処理、および画面遷移処理を実装

--- lib/src/features/auth/pages/login_page.dart
+++ lib/src/features/auth/pages/login_page.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sit_in_the_cafeteria/src/components/my_button.dart';
 import 'package:sit_in_the_cafeteria/src/components/my_textfield.dart';
 import 'package:sit_in_the_cafeteria/src/constant/form_category.dart';
+import 'package:sit_in_the_cafeteria/src/features/auth/pages/auth_state_notifier.dart';
 import 'package:sit_in_the_cafeteria/src/features/auth/pages/sign_up_page.dart';
+import 'package:sit_in_the_cafeteria/src/main_page.dart';
 
-class LoginPage extends HookWidget {
+class LoginPage extends HookConsumerWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     // コントローラー
     final TextEditingController usernameController = useTextEditingController(); // ユーザー名
     final TextEditingController passwordController = useTextEditingController(); // パスワード
 
+    // Validation用のフォームキー
     final formKey = GlobalKey<FormState>();
+
+    // エラーメッセージ
+    final errorMessage = useState<String>(" ");
+
+    // AuthState
+    final authState = ref.watch(authStateNotifierProvider);
+
+    // ログイン処理
+    void login(context) async {
+      if (formKey.currentState!.validate()) {
+        final username = usernameController.text;
+        final password = passwordController.text;
+
+        final authStateNotifier = ref.read(authStateNotifierProvider.notifier);
+
+        final bool result = await authStateNotifier.login(userName: username, password: password);
+
+        switch (result) {
+          // ログイン成功 -> メインページに遷移
+          case true:
+            Navigator.of(context).push(MaterialPageRoute(builder: (_) => const MainPage()));
+
+            break;
+
+          // ログイン失敗 -> エラーメッセージを表示
+          case false:
+            errorMessage.value = "ユーザー名またはパスワードが間違っています。";
+
+            break;
+        }
+      }
+    }
 
     return Scaffold(
       body: Center(
@@ -57,16 +93,37 @@ class LoginPage extends HookWidget {
               ),
 
               const SizedBox(
-                height: 60,
+                height: 20,
+              ),
+
+              // エラーメッセージ表示部分
+              if (errorMessage.value.isNotEmpty)
+                Text(
+                  errorMessage.value,
+                  style: const TextStyle(
+                    color: Colors.red,
+                    fontSize: 12,
+                  ),
+                ),
+
+              const SizedBox(
+                height: 20,
               ),
 
               // ログインボタン
               MyButton(
                 onPressed: () {
                   // ログイン処理
-                  if (formKey.currentState!.validate()) {}
+                  login(context);
                 },
-                child: const Text("ログイン"),
+                child: authState.when(
+                  data: (_) => const Text('ログイン'),
+                  error: (e, _) {
+                    errorMessage.value = e.toString();
+                    return const Text('ログイン');
+                  },
+                  loading: () => const CircularProgressIndicator(),
+                ),
               ),
 
               const SizedBox(
