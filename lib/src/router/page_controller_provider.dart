19a63a9042156cce886715e3499f2dfa0c8ed88a
hiropoo <125180473+hiropoo@users.noreply.github.com>
2024-07-17 15:18:43 +0900

    🔥 不要なProviderを削除

--- lib/src/router/page_controller_provider.dart
+++
@@ -1,8 +0,0 @@
-// pageControllerをグローバルに使えるようにするためのProvider
-
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final pageControllerProvider = Provider((ref) {
-  return PageController();
-});
