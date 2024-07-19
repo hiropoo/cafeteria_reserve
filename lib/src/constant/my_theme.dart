76b5e95970493818221e30999928a5fff2dd7d1f
hiropoo <125180473+hiropoo@users.noreply.github.com>
2024-07-19 21:31:10 +0900

    ⚡️ テーマのonSurfaceの色を変更

--- lib/src/constant/my_theme.dart
+++ lib/src/constant/my_theme.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme = ThemeData(
   colorScheme: const ColorScheme.light(
       surface: Color(0xFFF4F4F4), // 領域
-      onSurface: Color(0xFFD9D9D9), // 領域の上の領域
+      onSurface: Color(0xFF817F7F), // 領域の上の領域
       primary: Color(0xFF70C1CC), // メインカラー
       secondary: Color(0xFF3D3D3D), // ボタン
       tertiary: Color(0xFF81D762), // アクセント
