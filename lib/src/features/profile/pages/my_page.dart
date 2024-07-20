b9c3add79bd11974a3bc9c0fc9975e854c15faea
hiropoo <125180473+hiropoo@users.noreply.github.com>
2024-07-20 20:21:49 +0900

    ⚡️ マイページのユーザー情報の表示の実装
    - ユーザー名
    - ID
    - 友達数

--- lib/src/features/profile/pages/my_page.dart
+++ lib/src/features/profile/pages/my_page.dart
@@ -1,13 +1,193 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sit_in_the_cafeteria/src/components/my_container.dart';
+import 'package:sit_in_the_cafeteria/src/features/auth/pages/user_notifier.dart';
+import 'package:sit_in_the_cafeteria/src/features/profile/pages/friend_list_notifier.dart';
+import 'package:sit_in_the_cafeteria/src/router/app_router.dart';
 
-class MyPage extends StatelessWidget {
+class MyPage extends ConsumerWidget {
   const MyPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userNotifierProvider);
+    final friendList = ref.watch(friendListNotifierProvider);
+
+    return Scaffold(
       body: Center(
-        child: Text('My Page'),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              // ユーザー情報
+              MyContainer(
+                child: Column(
+                  children: [
+                    // 名前
+                    Text(
+                      user.userName,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.secondary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Divider(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // ID
+                        Material(
+                          color: Colors.transparent,
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Theme.of(context).colorScheme.onSurface,
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(10),
+                              onTap: () {
+                                // クリップボードにコピー
+                                Clipboard.setData(
+                                  ClipboardData(text: user.userID),
+                                );
+
+                                // スナックバー表示
+                                ScaffoldMessenger.of(context)
+                                  ..removeCurrentSnackBar()
+                                  ..showSnackBar(
+                                    const SnackBar(
+                                      content: Text('IDをコピーしました'),
+                                    ),
+                                  );
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(5),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'ID',
+                                      style: TextStyle(
+                                        color: Theme.of(context).colorScheme.secondary,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    Text(
+                                      user.userID,
+                                      style: TextStyle(
+                                        color: Theme.of(context).colorScheme.secondary,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 14,
+                                        height: 2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        const Spacer(),
+
+                        // 友達の数
+                        Material(
+                          color: Colors.transparent,
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Theme.of(context).colorScheme.onSurface,
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(10),
+                              onTap: () {
+                                // 友達一覧へ
+                                debugPrint('友達一覧へ');
+                                context.pushNamed(AppRoute.friendList.name);
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(5),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '友だち ',
+                                      style: TextStyle(
+                                        color: Theme.of(context).colorScheme.secondary,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    Row(
+                                      children: [
+                                        friendList.when(
+                                          data: (friendList) {
+                                            return Text(
+                                              friendList.length.toString(),
+                                              style: TextStyle(
+                                                color: Theme.of(context).colorScheme.secondary,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 20,
+                                              ),
+                                            );
+                                          },
+                                          loading: () => const Padding(
+                                            padding: EdgeInsets.fromLTRB(0, 9, 4, 9),
+                                            child: SizedBox(
+                                              width: 10,
+                                              height: 10,
+                                              child: CircularProgressIndicator(),
+                                            ),
+                                          ),
+                                          error: (error, stackTrace) => Text(
+                                            style: TextStyle(
+                                              color: Theme.of(context).colorScheme.secondary,
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 20,
+                                            ),
+                                            '?',
+                                          ),
+                                        ),
+                                        Text(
+                                          '人',
+                                          style: TextStyle(
+                                            color: Theme.of(context).colorScheme.secondary,
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              // 現在の予約
+            ],
+          ),
+        ),
       ),
     );
   }
